8fd6b3bc32a748873ed57bbaa09dec08c0cda8bc
ritul <rituljain.nitt@gmail.com>
2016-04-13 15:14:28 +0530

    print

--- public/assets/js/print.js
+++ public/assets/js/print.js
@@ -2,6 +2,11 @@ $(document).ready(function(){
 	$('.print').click(function(e){
 		// e.preventDefault();
 		var regNo = $(this).attr('data-reg');
-		window.location = 'http://localhost:8000/print/' + regNo;
+		var dept = regNo.split('/');
+		var categ = dept[0];
+		var applNo = dept[dept.length-1];
+
+		window.location = 'http://localhost:8000/print/' + categ + '/' + applNo;
+		console.log("hey");
 	}); 
  });
