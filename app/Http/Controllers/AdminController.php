237f1f1b9ea6701b757f2b1067c0bee320923a60
ritul <rituljain.nitt@gmail.com>
2016-05-08 09:25:28 +0530

    removed croppie

---
+++ app/Http/Controllers/AdminController.php
@@ -0,0 +1,370 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use View;
+use Validator;
+use Input;
+use PDF;
+use App\Http\Requests;
+use App\Http\Controllers\Controller;
+use App\Phd;
+use App\PhdUg;
+use App\PhdPg;
+use App\PhdOther;
+use App\PhdPro;
+use App\Ms;
+use App\MsUg;
+use App\MsScores;
+use App\MsPro;
+use App\Admin;
+use paginate;
+use Session;
+use File;
+use Log;
+
+use Illuminate\Support\Facades\Mail;
+
+
+class AdminController extends Controller
+{
+    public function login(Request $request)
+    {
+        $rules = array(
+            'username' => 'required',
+            'password' => 'required'
+        );
+
+        $validator = Validator::make($request->all(), $rules);
+
+        if($validator->fails())
+        {
+            $message = 'Please fill in all the details';
+            return View::make('error')->with('message', $message);
+        }
+        else
+        {
+            $username = $request->input('username');
+            $password = $request->input('password');
+
+            $auth = Admin::where('userName', $username)
+                ->where('password', ($password)) 
+                ->first();
+
+            if(count($auth) > 0)
+            {
+                Session::put('userName', $username);
+                Session::put('dept', $auth->dept);
+                return redirect('admin/home');
+            }
+            else
+            {
+                $message = 'Username or Password is incorrect';
+                return View::make('error')->with('message', $message);
+            }
+        }
+    }
+
+    public function change(Request $request)
+    {
+        $rules = array(
+            'username' => 'required',
+            'old_password' => 'required',
+        );
+
+        $validator = Validator::make($request->all(), $rules);
+
+        if($validator->fails())
+        {
+            $message = 'Please fill in all the details';
+            return View::make('error')->with('message', $message);
+        }
+        else
+        {
+            $username = $request->input('username');
+            $oldpassword = $request->input('old_password');
+            $newpassword = $request->input('new_password');
+
+            $auth = Admin::where('userName', $username)
+                ->where('password', sha1($oldpassword)) 
+                ->first();
+
+            if(count($auth) > 0)
+            {
+                Admin::where('userName', $username)
+                            ->where('password', sha1($oldpassword))
+                            ->update(['password' => sha1($newpassword)]);
+                Session::put('userName', $username);
+                Session::put('dept', $auth->dept);
+                return view('admin.home');
+            }
+            else
+            {
+                $message = 'Username or Password is incorrect';
+                return View::make('error')->with('message', $message);
+            }
+        }
+    }
+
+	public function adminView($phdormsc)
+	{
+        // Session::put('phdormsc', $phdormsc);
+        if(Session::get('dept') == 'all')
+        {
+            $rules1 = ['deleted' => false];
+            $rules2 = ['deleted' => false];
+            $rules3 = ['deleted' => false];
+        }
+        else
+        {
+            $rules1 = ['deleted' => false , 'dept1' => Session::get('dept')];
+            $rules2 = ['deleted' => false , 'dept2' => Session::get('dept')];
+            $rules3 = ['deleted' => false , 'dept3' => Session::get('dept')];
+        }
+        if($phdormsc == 'phd')
+        {
+            $candidates = Phd::where($rules1)
+                                        ->orWhere($rules2)
+                                        ->orWhere($rules3)
+                                        ->paginate(6);
+
+            $candidates_id = $candidates->lists('applNo');
+            $ugDetails = PhdUg::whereIn('applNo', $candidates_id)->get();
+            $pgDetails = PhdPg::whereIn('applNo', $candidates_id)->get(); 
+            $otherDetails = PhdOther::whereIn('applNo', $candidates_id)->get();
+            $proDetails = PhdPro::whereIn('applNo', $candidates_id)->get();
+            $data = array('candidates' => $candidates,
+                            'ug' => $ugDetails,
+                            'pg' => $pgDetails,
+                            'others' => $otherDetails,
+                            'pro' => $proDetails,
+                            'dept' => Session::get('dept')
+                            );
+            return View::make('admin.'.$phdormsc)->with('data', $data);
+        }
+        else
+        {
+            $candidates = Ms::where($rules1)
+                                        ->orWhere($rules2)
+                                        ->orWhere($rules3)
+                                        ->paginate(6);
+
+            $candidates_id = $candidates->lists('applNo');
+            $ugDetails = MsUg::whereIn('applNo', $candidates_id)->get(); 
+            $scores = MsScores::whereIn('applNo', $candidates_id)->get();//changed this from MsScores to MsSemScore 
+            $proDetails = MsPro::whereIn('applNo', $candidates_id)->get();
+            $data = array('candidates' => $candidates,
+                            'ug' => $ugDetails,
+                            'scores' => $scores,
+                            'pro' => $proDetails,
+                            'dept' => Session::get('dept')
+                            );
+            return View::make('admin.'.$phdormsc)->with('data', $data);
+        }
+
+	}
+
+	public function deleted(Request $request)
+	{	
+		$reg_number = $request->input('applNo');
+        $departments = explode('/', $reg_number);
+        $phdormsc = $departments[0];
+        if($phdormsc == 'PHD')
+        {
+            Phd::where('registrationNumber', $reg_number)
+                    ->update(['deleted' => true]);
+
+            $user = Phd::select('name', 'email')
+                                ->where('registrationNumber', $reg_number)
+                                ->first();                 
+            Mail::send('emails.delete', ['user' => $user->name], function ($m) use($user) {
+                $m->from('phdsection@nitt.edu', 'NITT Admissions');
+                $m->to($user->email, 'Applicant' )->subject('Greetings from NITT!');
+            });
+
+            return json_encode($reg_number);
+        }
+        else
+        {
+            Ms::where('applNo', $reg_number)
+                    ->update(['deleted' => true]);
+
+            $user = Ms::select('name', 'email')
+                                ->where('registrationNumber', $reg_number)
+                                ->first();
+            Mail::send('emails.delete', ['user' => $user->name], function ($m) use($user) {
+                $m->from('phdsection@nitt.edu', 'NITT Admissions');
+                $m->to($user->email, 'Applicant')->subject('Greetings from NITT!');
+            });
+
+            return json_encode($reg_number);
+        }
+	}
+
+    public function accepted(Request $request)
+    {
+        $reg_number = $request->input('applNo');
+        $departments = explode('/', $reg_number);
+        $phdormsc = $departments[0];
+        Log::info($phdormsc);
+        if($phdormsc == 'PHD')
+        {
+            Phd::where('registrationNumber', $reg_number)
+                    ->update(['accepted' => true]);
+
+            $user = Phd::select('name', 'email')
+                                ->where('registrationNumber', $reg_number)
+                                ->first();
+
+            Mail::send('emails.accept', ['user' => $user->name], function ($m) use($user) {
+                $m->from('phdsection@nitt.edu', 'NITT Admissions');
+                $m->to($user->email, 'Applicant')->subject('Greetings from NITT!');
+            });
+            
+            return json_encode($reg_number);
+        }
+        else
+        {
+            Ms::where('registrationNumber', $reg_number)
+                    ->update(['accepted' => true]);
+
+            $user = Ms::select('name', 'email')
+                                ->where('registrationNumber', $reg_number)
+                                ->first();
+            Log::info($reg_number);
+            Mail::send('emails.accept', ['user' => $user->name], function ($m) use($user) {
+                $m->from('phdsection@nitt.edu', 'NITT Admissions');
+                $m->to($user->email, 'Applicant')->subject('Greetings from NITT!');
+            });
+
+            return json_encode($reg_number);
+        }
+    }
+
+
+	public function printer($phdormsc, $reg_number)
+	{
+        $regNo = '';
+        $departments = explode('-', $reg_number);
+        for($i = 0; $i < sizeof($departments) - 1; $i++)
+        {
+            $regNo = $regNo.$departments[$i].'/';
+        }
+        $regNo = $regNo.$departments[sizeof($departments) - 1];
+	    if($phdormsc == 'PHD')
+        {
+            $candidates = Phd::where('registrationNumber', $regNo)
+                                ->first();
+            $applNo = $candidates->applNo;
+
+            if(!$candidates)
+            {
+                $message = 'Invalid registration number';
+                return View::make('error')->with('message', $message);
+            }                               
+
+            $ugDetails = PhdUg::where('applNo', $applNo)
+                                    ->first();
+            $pgDetails = PhdPg::where('applNo', $applNo)
+                                    ->first();
+            $otherDetails = PhdOther::where('applNo', $applNo)
+                                    ->first();
+            $proDetails = PhdPro::where('applNo', $applNo)
+                                    ->first();
+            $data = array('candidates' => $candidates,
+                            'ug' => $ugDetails,
+                            'pg' => $pgDetails,
+                            'others' => $otherDetails,
+                            'pro' => $proDetails,
+                            'phdorms' => $phdormsc
+                            );
+            $data['candidates']->phdormsc = $phdormsc;
+            $pdf = PDF::loadView('print', $data);
+            return response($pdf->output())
+                            ->header('Content-Type', 'application/pdf');
+        }
+        else
+        {
+            $candidates = Ms::where('registrationNumber', $regNo)
+                                ->first();
+
+            $applNo = $candidates->applNo;
+
+            if(!$candidates)
+            {
+                $message = 'Invalid registration number';
+                return View::make('error')->with('message', $message);
+            }                               
+            $ugDetails = MsUg::where('applNo', $applNo)
+                                    ->first();
+            $proDetails = MsPro::where('applNo', $applNo)
+                                    ->first();
+            $scores = MsScores::where('applNo', $applNo)
+                                    ->first();
+            $data = array('candidates' => $candidates,
+                            'ug' => $ugDetails,
+                            'scores' => $scores,
+                            'pro' => $proDetails,
+                            'phdorms' => $phdormsc
+                            );
+            $pdf = PDF::loadView('print', $data);
+            return response($pdf->output())
+                            ->header('Content-Type', 'application/pdf');
+        }
+	}
+
+    public function admitCard($phdormsc, $reg_number)
+    {
+        $regNo = '';
+        $departments = explode('-', $reg_number);
+        for($i = 0; $i < sizeof($departments) - 1; $i++)
+        {
+            $regNo = $regNo.$departments[$i].'/';
+        }
+        $regNo = $regNo.$departments[sizeof($departments) - 1];
+        $applNo = $departments[sizeof($departments) - 1];
+
+        $filename = $phdormsc.'/'.$applNo.'/'.$applNo;
+        $path = public_path() . '/uploads/' . $filename;
+        if(file_exists($path.'.jpg'))
+        {
+            $type = 'jpg';
+        }
+        else if(file_exists($path.'.jpeg'))
+        {
+            $type = 'jpeg';
+        }
+        else if(file_exists($path.'.png'))
+        {
+            $type = 'png';
+        }
+
+        if($phdormsc == 'PHD')
+        {
+            $candidate = Phd::select('name', 'registrationNumber')
+                            ->where('registrationNumber', $regNo)
+                            ->first();
+        }
+        else
+        {
+            $candidate = Ms::select('name', 'registrationNumber')
+                            ->where('registrationNumber', $regNo)
+                            ->first();
+        }
+
+        $data = array(
+            'image' => $phdormsc.'/'.$applNo.'/'.$applNo.'.'.$type,
+            'name' => $candidate->name,
+            'dept' => Session::get('dept'),
+            'regNo' => $candidate->registrationNumber
+        );
+        return view('admin.admit')->with($data);
+    }
+	
+    public function logout()
+    {
+        Session::flush();
+        return redirect('adminlogin');
+    }
+}
---
+++ resources/views/admin/phd.blade.php
@@ -0,0 +1,221 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta id="token" name="csrf-token" content="{{ csrf_token() }}">
+  <title>Admissions NITT | Admin Page</title>
+  <link rel="stylesheet" href="{{URL::asset('assets/css/style.css')}}">
+  <link rel="stylesheet" href="{{URL::asset('assets/css/materialize.min.css')}}">
+  <!-- <link rel="shortcut icon" href="{{URL::asset('assets/logo.jpg')}}"> -->
+  <link href="http://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+  <script src="{{URL::asset('assets/js/jquery-2.1.1.min.js')}}"></script>
+  <script src="{{URL::asset('assets/js/admin.js')}}"></script>
+  <script src="{{URL::asset('assets/js/materialize.min.js')}}"></script>
+  <script src="{{URL::asset('assets/js/print.js')}}"></script>
+  
+</head>
+<body>
+  
+<header>
+</header>
+
+<nav>
+    <div class="nav-wrapper ">
+      
+      <a href="#" data-activates="mobile-demo" class="button-collapse"><i class="material-icons">menu</i></a>
+      <ul class="hide-on-med-and-down">
+        <li><a href="home">Home</a></li>
+        <li><a href="ms">M.S. Applicants</a></li>
+        <li><a href="/logout">Logout</a></li>
+      </ul>
+      <ul class="side-nav" id="mobile-demo">
+       <li><a href="home">Home</a></li>
+        <li><a href="ms">M.S. Applicants</a></li>
+        <li><a href="/logout">Logout</a></li>
+      </ul>
+    </div>
+  </nav>
+
+  <div class="search">
+        <input type="text" id="search" placeholder="search" />
+  </div>
+  <div class="space-large"></div>
+  <div class="container">
+    <div class="candidates row">
+     @for($i = 0; $i < sizeof($data['candidates']); $i++)
+        @if(!$data['candidates'][$i]->deleted)
+        <div class="{!! $data['candidates'][$i]->applNo !!} col l5 offset-l1  " } data-reg = "{!! $data['candidates'][$i]->registrationNumber !!}">
+        @if($data['candidates'][$i]->accepted)
+        <div class="card center border">
+        @else
+        <div class="card center">
+        @endif
+          <div class="card-content">
+            <span class="card-title activator grey-text text-darken-4">Candidate's Card</span>
+            <div class="row">
+              
+                <p>Registration Number:  {!! $data['candidates'][$i]->registrationNumber !!}</p>
+              
+            </div>
+           
+            <div class="space-small">
+            </div>
+            <div class="center">
+              <a class="waves-effect waves-light btn modal-trigger" href="#modal{!! 0 !!}">Click Here To view full form</a>
+            </div>
+          </div>
+          </div>
+
+          <div id="modal{!! 0 !!}" class="modal l8">
+          <div class="modal-content">
+          <h4>Candidate's Form</h4>
+          
+          <p><b>Registration Number:</b> {!! $data['candidates'][$i]->registrationNumber !!}</p>
+          <p><b>Date of Registration:</b> {!! $data['candidates'][$i]->dateOfReg !!}
+          </p>
+         
+          <p><b>Application Category:</b> {!! $data['candidates'][$i]->applicationCategory !!}</p>
+          <p><b>Department:</b> {!! $data['candidates'][$i]->dept !!}</p>
+          <p><b>Email Id:</b> {!! $data['candidates'][$i]->email !!}</p>
+          <p><b>Area of Research:</b> {!! $data['candidates'][$i]->areaOfResearch !!}</p>
+          <p><b>Name of Candidate:</b> {!! $data['candidates'][$i]->
+          name !!}</p>
+          <p><b>Email Id:</b> {!! $data['candidates'][$i]->email !!}</p>
+          <p><b>Father's Name:</b> {!! $data['candidates'][$i]->fatherName !!}</p>
+          <p><b>Date of Birth:</b> {!! $data['candidates'][$i]->dob !!}</p>
+          <p><b>Category:</b> {!! $data['candidates'][$i]->category !!}</p>
+          <p><b>Sex :</b> {!! $data['candidates'][$i]->sex !!}</p>
+          <p><b>Marital Status: </b>{!! $data['candidates'][$i]->maritalStatus !!}</p>
+          <p><b>Physically Handicapped:</b> {!! $data['candidates'][$i]->PH !!}</p><!-- wtf -->
+          <p><b>Nationality:</b> {!! $data['candidates'][$i]->nationality !!}</p>
+          <p><b>Address for Communication:</b> {!! $data['candidates'][$i]->addrforcomm !!}</p>
+          <p><b>Permanent Address:</b> {!! $data['candidates'][$i]->permanentaddr !!}</p>
+          <p><b>Mobile Number:</b> {!! $data['candidates'][$i]->mobile !!}</p>
+          <p><b>Landline Number:</b> {!! $data['candidates'][$i]->landline !!}</p>
+
+          <p><b>Undergraduate Degree:</b> {!! $data['ug'][$i]->degreeName !!}</p>
+          <p><b>Undergraduate Branch:</b> {!! $data['ug'][$i]->branch !!}</p>
+          <p><b>Undergraduate G.P.A:</b> {!! $data['ug'][$i]->gpa !!}</p>
+          <p><b>Undergraduate Class:</b> {!! $data['ug'][$i]->class !!}</p>
+          <p><b>Undergraduate Name of Institution:</b> {!! $data['ug'][$i]->institutionName !!}</p>
+          <p><b>Undergraduate Name of University:</b> {!! $data['ug'][$i]->universityName !!}</p>
+          <p><b>Undergraduate Year of Passing:</b> {!! $data['ug'][$i]->yop !!}</p>
+
+          <p><b>Post-graduate Degree:</b> {!! $data['pg'][$i]->degreeName !!}</p>
+          <p><b>Post-graduate Branch:</b> {!! $data['pg'][$i]->branch !!}</p>
+          <p><b>Post-graduate G.P.A:</b> {!! $data['pg'][$i]->gpa !!}</p>
+          <p><b>Post-graduate Class:</b> {!! $data['pg'][$i]->class !!}</p>
+          <p><b>Post-graduate Name of Institution:</b> {!! $data['pg'][$i]->institutionName !!}</p>
+          <p><b>Post-graduate Name of University:</b> {!! $data['pg'][$i]->universityName !!}</p>
+          <p><b>Post-graduate Year of Passing:</b> {!! $data['pg'][$i]->yop !!}</p>
+          @if($data['others'][$i]->exam)
+          <p><b>Exam Name:</b> {!! $data['others'][$i]->exam !!}</p>
+          @endif
+          <p><b>Discipline:</b> {!! $data['others'][0]->discipline !!}</p>
+          <p><b>Score:</b> {!! $data['others'][0]->score !!}</p>
+          <p><b>Rank:</b> {!! $data['others'][0]->rank !!}</p>
+          <p><b>Title of Project:</b> {!! $data['others'][0]->pgproject !!}</p>
+          <p><b>Details of Publication 1:</b> {!! $data['others'][0]->publications1 !!}</p>
+          <p><b>Details of Publication 2:</b> {!! $data['others'][0]->publications2 !!}</p>
+          <p><b>Details of Publication 3:</b> {!! $data['others'][0]->publications3 !!}</p>
+          <p><b>Awards1:</b> {!! $data['others'][0]->awards1 !!}</p>
+          <p><b>Awards2:</b> {!! $data['others'][0]->awards2 !!}</p>
+          <p><b>Awards3:</b> {!! $data['others'][0]->awards3 !!}</p>
+
+          <p><b>Employer Details 1:</b> {!! $data['pro'][0]->proexp1 !!}</p>
+          <p><b>Position at Work 1:</b>{!! $data['pro'][0]->position1 !!}</p>
+          <p><b>From 1:</b>{!! $data['pro'][0]->from1 !!}</p>
+          <p><b>To 1:</b>{!! $data['pro'][0]->to1 !!}</p>
+          <p><b>Employer Details 2:</b> {!! $data['pro'][0]->proexp2 !!}</p>
+          <p><b>Position at Work 1:</b>{!! $data['pro'][0]->position2 !!}</p>
+          <p><b>From 1:</b>{!! $data['pro'][0]->from2 !!}</p>
+          <p><b>To 1:</b>{!! $data['pro'][0]->to2 !!}</p>
+          <p><b>Employer Details 3:</b> {!! $data['pro'][0]->proexp3 !!}</p>
+          <p><b>Position at Work 1:</b>{!! $data['pro'][0]->position3 !!}</p>
+          <p><b>From 1:</b>{!! $data['pro'][0]->from3 !!}</p>
+          <p><b>To 1:</b>{!! $data['pro'][0]->to3 !!}</p>
+          
+          <p><b>Submission Date:</b> {!! $data['others'][0]->subdate !!}</p>
+        </div>
+      <div class="modal-footer center">
+        @if($data['dept'] == 'all')
+        <div class="col s12">
+          <a href="#!" data-reg={!! $data['candidates'][0]->registrationNumber!!} class="discard1 btn modal-action modal-close waves-effect waves-green btn-flat">Discard</a>
+        @if(!$data['candidates'][0]->accepted)
+        <a href="#!"  data-reg={!! $data['candidates'][0]->registrationNumber!!} class="accept btn modal-action modal-close waves-effect waves-green btn-flat">Accept</a>
+        @endif
+        </div>
+        @endif
+        <div class="col s12">
+        <a href="#!" data-reg={!! $data['candidates'][0]->registrationNumber!!} class="print btn modal-action modal-close waves-effect waves-green btn-flat">Print</a>
+        <a href="#!" data-reg={!! $data['candidates'][0]->registrationNumber!!} class="admit btn modal-action modal-close waves-effect waves-green btn-flat">Admit</a>
+        </div>
+      </div>
+      </div>
+        
+    </div>
+          @endif
+          @endfor
+
+           </div>
+           <div class="center">
+      <ul class="pagination">
+    <li class="disabled"><a href={!! $data["candidates"]->previousPageUrl()!!}><i class="material-icons">chevron_left</i></a></li>
+    <li class="active teal" ><a href="#!">{!! $data["candidates"]->currentPage() !!}</a></li>
+    <li class="waves-effect"><a href={!! $data["candidates"]->nextPageUrl()!!}><i class="material-icons">chevron_right</i></a></li>
+  </ul>
+  </div>
+
+   <div class="space-medium"></div>
+<div class="center">
+   <a class="waves-effect waves-light btn" href="ms">View M.S. applicants</a>
+  </div>
+   </div>
+<div class="space-large"></div>
+
+<footer class="page-footer teal darken-4">
+          <div class="container">
+            <div class="row">
+              <div class="col l6 s12">
+                <h5 class="white-text">Contact Us</h5>
+                <p class="grey-text text-lighten-4">National Institute of Technology</p>
+                <p class="grey-text text-lighten-4">Tiruchirappalli - 620015</p>
+                <p class="grey-text text-lighten-4">Tamil Nadu, INDIA</p>
+              </div>
+              <div class="col l4 offset-l2 s12">
+                <h5 class="white-text">QuickLinks</h5>
+                <ul>
+                  <li><a class="grey-text text-lighten-3" href="http://www.nitt.edu/">Institute Website</a></li>
+                  <li><a class="grey-text text-lighten-3" href="http://www.nitt.edu/home/academics/departments/">Departments</a></li>
+                  <li><a class="grey-text text-lighten-3" href="http://www.nitt.edu/home/admissions/">Admissions</a></li>
+                  <li><a class="grey-text text-lighten-3" href="http://www.nitt.edu/contact">Contact Us</a></li>
+                </ul>
+              </div>
+            </div>
+          </div>
+          <div class="footer-copyright dark">
+            <div class="container center">
+            
+            <a class="grey-text text-lighten-4" href="#!">Made with &hearts; by Delta Force</a>
+            </div>
+          </div>
+        </footer>
+            
+
+  <script type="text/javascript">
+  $(document ).ready(function(){
+    $(".button-collapse").sideNav();
+  })
+  </script>    
+
+  <script type="text/javascript">
+      $(document).ready(function(){
+    // the "href" attribute of .modal-trigger must specify the modal ID that wants to be triggered
+    $('.modal-trigger').leanModal();
+  });
+  </script>
+
+</body>
+</html>
