4761896dfd343536bd74e1de0fdea6d7d283cfbd
Parth Thakkar <thakkarparth007@gmail.com>
2016-05-13 00:17:03 +0530

    Change Spellings

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -44,7 +44,7 @@
   <div class="space-medium"></div>
   <div class="container center main">
     <p class="large">
-      Welcome to NITT PhD. and M.S. application portal!
+      Welcome to NIT Trichy PhD. and M.S. (by research) application portal!
     </p>
     <div class="space-small"></div>
     <!-- <p>The Ph.D. degree is granted for distinction attained in a special field of concentration, by the demonstrated ability to modify or enlarge a significant subject in a dissertation, based upon original research scholarship. The minimum requirement is that the doctoral student follow a unified and organized program of study.</p> -->
@@ -52,8 +52,8 @@
   	<div class="row">
       
       <div class="col l12 s12">
-        <a class="waves-effect waves-light btn" href="/phdinstructions">Click here for PhD Application</a>
-        <a class="waves-effect waves-light btn" href="/msinstructions">Click here for M.S Application</a>
+        <a class="waves-effect waves-light btn" href="/phdinstructions">Click here for <span style="text-transform: initial">PhD.</span> Application</a>
+        <a class="waves-effect waves-light btn" href="/msinstructions">Click here for M.S. Application</a>
         </div>
         <div class="space-medium"> </div>
         <div class="col l12 s12">
